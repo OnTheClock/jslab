a5cf629084bea7538e79d3a62693eb8472ab8704
OnTheClock <joel@raspi1>
2018-08-23 20:04:12 -0400

    adjusted organization

---
+++ scripts/wikibackup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+# Variables
+
+wikiDB=pi_wiki
+wikiDBuser=mwki
+wikidir=/var/www/html/mediawiki
+timestamp=`date +%y.%m.%d-%H.%M`
+mkdir "$HOME/wikibackup/$timestamp"
+backupdir="$HOME/wikibackup/$timestamp"
+dbdump="$backupdir/wiki-$timestamp.sql.gz"
+xmldump="$backupdir/wiki-$timestamp.xml.gz"
+filedump="$backupdir/wiki-$timestamp.files.tar.gz"
+
+echo "Wiki backup. Database: $wikiDB; User: $wikiDBuser; Directory: $wikidir; Backup to: $backupdir"
+echo
+echo "creating database dump $dbdump..."
+mysqldump --user=$wikiDBuser $wikiDB | gzip > "$dbdump"
+echo "Database dump complete"
+
+
+echo
+echo "creating XML dump $xmldump..."
+cd "$wikidir/maintenance"
+php -d error_reporting=E_ERROR dumpBackup.php --full | gzip > "$xmldump"
+echo "XML dump complete"
+
+
+echo
+echo "creating file archive $filedump..."
+cd "$HOME"
+tar -czvf "$filedump" "$wikidir"
+echo "Filesystem backup complete"
+echo
+echo "Wiki backup complete"
