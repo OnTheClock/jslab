c963d9d9fe387c95cfca58e7f9b331dd868b6dca
Joel Kolterjahn <jtkolterjahn@gmail.com>
2022-02-09 21:25:40 -1000

    Add files via upload

---
+++ scripts/webmin.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update
+
+sudo echo deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
+
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
+sudo apt update
+sudo apt install webmin -y
